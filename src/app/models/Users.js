d79faa429bbde536c8694dcaff4b9992a2538133
lucas nascimento <lucaspnascimento@hotmail.com>
2019-07-02 13:33:27 -0300

    config files

---
+++ src/app/models/Users.js
@@ -0,0 +1,35 @@
+import { Model, Sequelize } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        endereco: Sequelize.STRING,
+        cpf: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // nunca existirá na base de dados  campos VIRTUAL
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+}
+
+export default User;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0A6025B566E487D465E231783DFE1550',
+  expiresIn: '7d',
+};
